addd834cf1f8a7071125f4a043b73fa1f5f1ce67
hashlog5 <108911663+hashlog5@users.noreply.github.com>
2022-11-07 19:36:08 -0800

    submitting guesses

--- src/hooks/useWordle.jsx
+++ src/hooks/useWordle.jsx
@@ -4,36 +4,46 @@ const useWordle = (solution) => {
   const [turn, setTurn] = useState(0);
   const [currentGuess, setCurrentGuess] = useState('');
   const [guesses, setGuesses] = useState([]); // each guess is an array
-  const [history, setHistory] = useState([]); // each guess is a string
+  const [history, setHistory] = useState(['hello', 'ninja']); // each guess is a string
   const [isCorrect, setIsCorrect] = useState(false);
 
-  const formatGuess = () => {};
+  const formatGuess = () => {
+    console.log('formatting the guess - ', currentGuess);
+  };
 
   const addNewGuess = () => {};
 
   const handleKeyup = ({ key }) => {
-    if (key === 'Backspace') {
-      setCurrentGuess((prev) => {
-        return prev.slice(0, -1);
-      });
+    if (key === 'Enter') {
+      if (turn > 5) {
+        console.log('you used all your guesses!');
+        return;
+      }
 
+      if (history.includes(currentGuess)) {
+        console.log('you already tried that word.');
+        return;
+      }
+
+      if (currentGuess.length !== 5) {
+        console.log('word must be 5 chars.');
+        return;
+      }
+
+      formatGuess();
+    }
+
+    if (key === 'Backspace') {
+      setCurrentGuess((prev) => prev.slice(0, -1));
       return;
     }
 
     if (/^[A-Za-z]$/.test(key) && currentGuess.length < 5) {
-      setCurrentGuess((prev) => {
-        return prev + key;
-      });
+      setCurrentGuess((prev) => prev + key);
     }
   };
 
-  return {
-    turn,
-    currentGuess,
-    guesses,
-    isCorrect,
-    handleKeyup,
-  };
+  return { turn, currentGuess, guesses, isCorrect, handleKeyup };
 };
 
 export default useWordle;
