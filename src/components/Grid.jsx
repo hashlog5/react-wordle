fb591b967903e5328ca9e0c67138b9fc7e6ac18e
hashlog5 <108911663+hashlog5@users.noreply.github.com>
2022-11-08 19:38:37 -0800

    show current guess

--- src/components/Grid.jsx
+++ src/components/Grid.jsx
@@ -4,6 +4,10 @@ const Grid = ({ currentGuess, guesses, turn }) => {
   return (
     <div>
       {guesses.map((g, i) => {
+        if(turn === i){
+          return <Row key={i} currentGuess={currentGuess} />
+        }
+
         return <Row key={i} guess={g} />;
       })}
     </div>
--- src/components/Row.jsx
+++ src/components/Row.jsx
@@ -1,4 +1,4 @@
-const Row = ({ guess }) => {
+const Row = ({ guess, currentGuess }) => {
   if (guess) {
     return (
       <div className='row past'>
@@ -6,6 +6,24 @@ const Row = ({ guess }) => {
           <div key={i} className={l.color}>
             {l.key}
           </div>
+        ))}
+      </div>
+    );
+  }
+
+  if (currentGuess) {
+    let letters = currentGuess.split('');
+
+    return (
+      <div className='row current'>
+        {letters.map((letter, i) => (
+          <div key={i} className='filled'>
+            {letter}
+          </div>
+        ))}
+
+        {[...Array(5 - letters.length)].map((_, i) => (
+          <div key={i}></div>
         ))}
       </div>
     );
